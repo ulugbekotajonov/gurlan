8efe418776082fd60e4af3ac6dc9cc9cd1bff612
ulugbekotajonov <ulugbekotajonov711@gmail.com>
2026-05-20 08:27:35 +0500

    Add view template

---
+++ resources/views/components/head.blade.php
@@ -0,0 +1,24 @@
+<meta charset="utf-8">
+
+<title>Gurlan tumani tarixi</title>
+
+<meta name="description" content="Description">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2">
+
+<link rel="icon" href="{{ asset("assets/img/favicon/favicon.ico") }}" type="image/x-icon">
+
+<link rel="stylesheet" href="{{ asset("assets/css/bootstrap-grid.css") }}">
+<link rel="stylesheet" href="{{ asset("assets/css/style.css") }}">
+<link rel="preload" href="{{ asset("assets/fonts/source-sans-pro-v21-latin/source-sans-pro-v21-latin-regular.woff2") }}"
+      as="font" type="font/woff2" crossorigin>
+<link rel="preload" href="{{ asset("assets/fonts/source-sans-pro-v21-latin/source-sans-pro-v21-latin-700.woff2") }}"
+      as="font" type="font/woff2" crossorigin>
+<link rel="preload" href="{{ asset("assets/fonts/montserrat-v25-latin/montserrat-v25-latin-700.woff2") }}" as="font"
+      type="font/woff2" crossorigin>
+<link rel="preload" href="{{ asset("assets/fonts/montserrat-v25-latin/montserrat-v25-latin-600.woff2") }}" as="font"
+      type="font/woff2" crossorigin>
+<link rel="preload" href="{{ asset("assets/fonts/material-icons/material-icons.woff2") }}" as="font" type="font/woff2"
+      crossorigin>
+<link rel="preload" href="{{ asset("assets/fonts/material-icons/material-icons-outlined.woff2") }}" as="font"
+      type="font/woff2" crossorigin>
